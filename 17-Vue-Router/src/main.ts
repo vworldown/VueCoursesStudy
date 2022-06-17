0e330a04e487259f5078de4a3c9e8a6ebf0a8a7b
V <2583695112@qq.com>
2022-06-17 16:31:06 +0800

    feat: route guards

--- 17-Vue-Router/src/main.ts
+++ 17-Vue-Router/src/main.ts
@@ -2,4 +2,10 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import { router } from "./router/router";
 
+router.beforeEach((to, from, next) => {
+  console.log("Global beforEach");
+  console.log(to, from);
+  next();
+});
+
 createApp(App).use(router).mount("#app");
